3ddd6530f2868b24eb0d19c482c791b2df2fc6da
jumashovnursultan <nursultanjumashovv@gmail.com>
2023-09-20 09:03:39 +0600

    home appBar

--- lib/home_scree.dart
+++ lib/home_scree.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return ColoredBox(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 70,
+            leadingWidth: 58,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: SvgPicture.asset('assets/icons/logo.svg'),
             ),
-          ],
+            backgroundColor: const Color(0xFFF4F4F4),
+            title: const Text('dev'),
+            titleSpacing: 10,
+            centerTitle: false,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset('assets/icons/menu.svg'),
+              ),
+            ],
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
         ),
       ),
     );
