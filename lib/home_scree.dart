8458d760a9e3efa6b057338b3f9136d5bcdcfe7b
jumashovnursultan <nursultanjumashovv@gmail.com>
2023-09-27 08:44:33 +0600

    my skillls

--- lib/home_scree.dart
+++ lib/home_scree.dart
@@ -112,7 +112,19 @@ class HomeScreen extends StatelessWidget {
                         TitleTextWidget(
                           firstText: 'My',
                           secondText: 'Skills',
-                        )
+                        ),
+                        SizedBox(height: 40),
+                        HardSkillsWidget(
+                          text: 'Flutter: Dart',
+                        ),
+                        SizedBox(height: 40),
+                        HardSkillsWidget(
+                          text: 'Android: Kotlin',
+                        ),
+                        SizedBox(height: 70),
+                        TitleTextWidget(
+                          firstText: 'Last projects',
+                        ),
                       ],
                     ),
                   ),
@@ -122,6 +134,43 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class HardSkillsWidget extends StatelessWidget {
+  const HardSkillsWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 40,
+          width: 145,
+          color: const Color(0xFFF4F4F4),
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        const LinearProgressIndicator(
+          minHeight: 15,
+          value: ((1 * 1) / 3),
+          color: Color(0xFF007AFF),
+          backgroundColor: Color(0xFFB2D7FF),
+        ),
+      ],
     );
   }
 }
